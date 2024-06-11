9cc92f3c5b7e08e28d6fa63113503091e04bd7bb
Tesyaf <abraralif3@gmail.com>
2024-06-11 13:11:23 +0700

    menambah is valid password

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,7 @@
 #include<windows.h>
 #include<fstream>
 #include<string>
+#include<cstring>
 using namespace std;
 
 #define SCREEN_WIDTH 90
@@ -26,8 +27,31 @@ void gotoxy(int x, int y)
 	SetConsoleCursorPosition(Console, CursorPosition);
 }
 
+bool isValidPassword(const char * password){
+    int upper = 0;
+    int lower = 0;
+    int angka = 0;
+    int n = strlen(password);
+    if(n<3 || n>=50){
+        return 0;
+    }for (int i = 0;i<n;i++){
+        if(islower(password[i])){
+            lower++;
+        }else if(isupper(password[i])){
+            upper++;
+        }else if(isdigit(password[i])){
+            angka++;
+        }
+    }
+    if(upper == 0 || lower == 0 || angka == 0){
+        return 0;
+    }
+    return 1;
+}
+
 void Login(){
-    string pass, un, Password, UserName;
+    string pass, un, UserName;
+    char Password[50];
     bool status = false;
 
     cout << "LOGIN ^.^\n\n";
@@ -53,13 +77,20 @@ void Login(){
 }
 
 void SignUp(){
-    string UserName, Password,ConfirmPassword;
-
+    string UserName, ConfirmPassword;
+    char Password[50];
     cout << "SIGN UP ^.^\n\n";
     cout << "Username\t\t: ";
     cin >> UserName;
     cout << "Password\t\t: ";
     cin >> Password;
+
+    while(!isValidPassword(Password)){
+        cout <<"Password setidaknya harus memiliki 1 huruf besar, 1 huruf kecil dan 1 angka !!"<<endl;
+        cout <<"Masukkan Password: ";
+        cin >> Password;
+    }
+
     cout << "Confirm Your Password\t: ";
     cin >> ConfirmPassword;
 
