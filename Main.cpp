42d6aa9b47ff9c6f692ef3735a62b2a2b0aeb7bd
Tesyaf <abraralif3@gmail.com>
2024-06-12 01:21:10 +0700

    merge sort

--- Main.cpp
+++ Main.cpp
@@ -49,6 +49,54 @@ void LowerCase(std::string& inputString) {
     }
 }
 
+void merge(vector<string>& arr, int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    vector<string> L(n1), R(n2);
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(vector<string>& arr, int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
 class User {
 private:
     string UserName, Password;
@@ -89,17 +137,27 @@ public:
     }
 
     void viewTransactionHistory() {
-        ifstream file(UserName + "_history.txt");
-        string line;
-        if (file.is_open()) {
-            while (getline(file, line)) {
-                cout << line << endl;
-            }
-            file.close();
-        } else {
-            cout << "No transaction history found." << endl;
+    ifstream file(UserName + "_history.txt");
+    string line;
+    vector<string> transactions;
+
+    if (file.is_open()) {
+        while (getline(file, line)) {
+            transactions.push_back(line);
         }
+        file.close();
+
+        // Sort the transactions using merge sort
+        mergeSort(transactions, 0, transactions.size() - 1);
+
+        // Display the sorted transactions
+        for (const string& transaction : transactions) {
+            cout << transaction << endl;
+        }
+    } else {
+        cout << "No transaction history found." << endl;
     }
+}
 };
 
 bool isValidPassword(const char * password){
