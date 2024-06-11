7bc29eb551ed5f107559f45c69e29268f04bd6d0
Tesyaf <abraralif3@gmail.com>
2024-06-11 23:29:16 +0700

    Banyak dah

--- Main.cpp
+++ Main.cpp
@@ -7,7 +7,7 @@
 #include<map>
 #include<ctime>
 #include <vector>
-#include <algorithm>
+#include<algorithm>
 
 using namespace std;
 
@@ -24,7 +24,6 @@ using namespace std;
 
 HANDLE Console = GetStdHandle(STD_OUTPUT_HANDLE);
 COORD CursorPosition;
-
 
 void gotoxy(int x, int y)
 {
@@ -43,53 +42,57 @@ void LowerCase(std::string& inputString) {
     }
 }
 
-class User{
-    private:
-        string UserName, Password; 
-        vector<string> transaction_history;
-    public:
-        User(string UserName, string Password){
-            this -> UserName = UserName;
-            this -> Password = Password;
-        }
+class User {
+private:
+    string UserName, Password;
+    vector<string> transaction_history;
 
-        void setUserName(string UserName){
-            this -> UserName = UserName;
-        }
+public:
+    User(string UserName, string Password) {
+        this->UserName = UserName;
+        this->Password = Password;
+    }
 
-        void setPassword(string Password){
-            this -> Password = Password;
-        }
+    void saveToFile() {
+        ofstream file("dataAcc.txt", ios::app);
+        file << UserName << " " << Password << endl;
+        file.close();
+    }
 
-        string getUserName(){
-            return UserName;
-        }
-
-        string getPassword(){
-            return Password;
-        }
-  
-        void addTransaction(const string& transaction) {
-            transaction_history.push_back(transaction);
-            ofstream file;
-            file.open(UserName + "_history.txt", ios::app);
-            file << transaction << endl;
-            file.close();
-        }
-
-        void viewTransactionHistory() {
-            ifstream file(UserName + "_history.txt");
-            string line;
-            if (file.is_open()) {
-                while (getline(file, line)) {
-                    cout << line << endl;
-                }
-                file.close();
-            } else {
-                cout << "No transaction history found." << endl;
+    static bool verifyLogin(string UserName, string Password) {
+        string un, pass;
+        ifstream read("dataAcc.txt");
+        while (read >> un >> pass) {
+            if (un == UserName && pass == Password) {
+                return true;
             }
         }
-  
+        return false;
+    }
+
+    string getUserName() {
+        return UserName;
+    }
+
+    void addTransaction(const string& transaction) {
+        transaction_history.push_back(transaction);
+        ofstream file(UserName + "_history.txt", ios::app);
+        file << transaction << endl;
+        file.close();
+    }
+
+    void viewTransactionHistory() {
+        ifstream file(UserName + "_history.txt");
+        string line;
+        if (file.is_open()) {
+            while (getline(file, line)) {
+                cout << line << endl;
+            }
+            file.close();
+        } else {
+            cout << "No transaction history found." << endl;
+        }
+    }
 };
 
 bool isValidPassword(const char * password){
@@ -114,36 +117,25 @@ bool isValidPassword(const char * password){
     return 1;
 }
 
-bool Login(){
-    string pass, un, UserName;
-    char Password[50];
-    bool status = false;
-
+bool Login(User& currentUser) {
+    string UserName, Password;
     cout << "LOGIN ^.^\n\n";
     cout << "Username\t: ";
     cin >> UserName;
     cout << "Password\t: ";
     cin >> Password;
 
-    ifstream read("dataAcc.txt");
-    while(read >> un >> pass){
-        if(un == UserName && pass == Password){
-        status = true;
-        }
-        else status = false;
-    }
-
-    if(status == true){ 
-        cout << "Welcome " << UserName <<"!\n";
-        return 1;
-    }
-    else{
+    if (User::verifyLogin(UserName, Password)) {
+        currentUser = User(UserName, Password);
+        cout << "Welcome " << UserName << "!\n";
+        return true;
+    } else {
         cout << "Error! Please check your username and password!\n";
-        return 0;
-    }   
+        return false;
+    }
 }
 
-void SignUp(){
+void SignUp() {
     string UserName, ConfirmPassword;
     char Password[50];
     cout << "SIGN UP ^.^\n\n";
@@ -152,28 +144,27 @@ void SignUp(){
     cout << "Password\t\t: ";
     cin >> Password;
 
-    while(!isValidPassword(Password)){
-        cout <<"Password must contain lower case, upper case, and number!"<<endl;
-        cout <<"Password\t\t: ";
+    while (!isValidPassword(Password)) {
+        cout << "Password must contain lower case, upper case, and number!" << endl;
+        cout << "Password\t\t: ";
         cin >> Password;
     }
 
     cout << "Confirm Your Password\t: ";
     cin >> ConfirmPassword;
 
-    while(Password != ConfirmPassword){
+    while (Password != ConfirmPassword) {
         cout << "Passwords do not match. Please try again.\n";
         cout << "Confirm Your Password\t: ";
         cin >> ConfirmPassword;
     }
 
-    ofstream file("dataAcc.txt", ios::app);
-    file << UserName << " " << Password << endl;
-    //system("cls");
-    cout << "\nSigning Up succesfull\n";
+    User newUser(UserName, Password);
+    newUser.saveToFile();
+    cout << "\nSigning Up successful\n";
 }
 
-void Catalogue(){
+void Catalogue(User& currentUser){
   // 1. Cheese Cake
   // 2. Tart
   // 3. Black Forest
@@ -221,61 +212,52 @@ topping["raspberries"] = 4000;
 topping["hazelnuts"] = 5000;
 
   
-    cout <<"1. " << cakeName.at(0) << endl;
+    cout <<"1. Cheese Cake\n";
     cout << "A cake with a soft texture and rich cheese flavor, usually made with cream cheese, eggs, and sugar."<<endl;
     cout <<"Price\t: " << cake.at(1) << " IDR" << endl;
     cout <<"Size\t: _________________________________ S/M/L/XL\n"; 
     cout <<"Topping\t: Sprinkle,Cheese,White Choco\n\n\n";
-
-    cout <<"2. " << cakeName.at(1) << endl;
+    cout <<"2. Tart\n";
     cout <<"A cake with a crispy pastry base and various fillings such as fruits, custard, or chocolate."<<endl;
     cout <<"Price\t:" << cake.at(2) << " IDR" << endl;
     cout <<"Size_________________________________ S/M/L/XL\n";
     cout <<"Topping\t: Strawberry,Cherry,Raspberries\n\n\n";
-
-    cout <<"3. " << cakeName.at(2) << endl;
+    cout <<"3. Black Forest\n";
     cout <<"A layered cake consisting of chocolate cake layers, whipped cream, and cherries."<<endl;
     cout <<"Price\t:" << cake.at(3) << " IDR" << endl;
     cout <<"Size\t: _________________________________ S/M/L/XL\n";
     cout <<"Topping\t: Dark Choco,Chocolatte Chips,Almond\n\n\n";
-
-    cout <<"4. " << cakeName.at(3) << endl;
+    cout <<"4. Butter Cake\n";
     cout <<"A dense and soft cake with a strong buttery flavor."<<endl;
     cout <<"Price\t:" << cake.at(4) << " IDR" << endl;
     cout <<"Size\t: _________________________________ S/M/L/XL\n";
     cout <<"Topping\t: Marshmallows,Whipped Cream,Almond\n\n\n";
-
-    cout <<"5. " << cakeName.at(4) << endl;
+    cout <<"5. Pound Cake\n";
     cout << "A dense and soft cake, usually made with equal parts of four main ingredients: flour, butter, eggs, and sugar."<<endl;
     cout <<"Price\t:" << cake.at(5) << " IDR" << endl;
     cout <<"Size\t: _________________________________ S/M/L/XL\n";
     cout <<"Topping\t: chocolateChips,hazelnuts,cherry\n\n\n";
-
-    cout <<"6. " << cakeName.at(5) << endl;
+    cout <<"6. Chiffon Cake\n";
     cout <<"A light and soft cake made from vegetable oil, eggs, sugar, flour, baking powder, and flavoring."<<endl;
     cout <<"Price\t:" << cake.at(6) << " IDR" << endl;
     cout <<"Size\t: _________________________________ S/M/L/XL\n";
     cout <<"Topping\t: strawberry,marshmallows,sprinkle\n\n\n";
-
-    cout <<"7. " << cakeName.at(6) << endl;
+    cout <<"7. Red Velvet\n";
     cout <<"A red-colored cake with a light chocolate flavor, usually layered with cream cheese frosting."<<endl;
     cout <<"Price\t:" << cake.at(7) << " IDR" << endl;
     cout <<"Size\t: _________________________________ S/M/L/XL\n";
     cout <<"Topping\t: whippedcream,cheese,raspberries\n\n\n";
-    
-    cout <<"8. " << cakeName.at(7) << endl;
+    cout <<"8. Flourless Cake\n";
     cout <<"A dense and soft chocolate cake made without flour, often having an intense chocolate flavor."<<endl;
     cout <<"Price\t:" << cake.at(8) << " IDR" << endl;
     cout <<"Size\t: _________________________________ S/M/L/XL\n";
     cout <<"Topping\t: cherry,darkchoco,sprinkle\n\n\n";
-
-    cout <<"9. " << cakeName.at(8) << endl;
+    cout <<"9. Victoria Sponge Cake\n";
     cout <<"A classic English cake consisting of two sponge cake layers filled with strawberry or raspberry jam and whipped cream."<<endl;
     cout <<"Size\t: _________________________________ S/M/L/XL\n";
     cout <<"Price\t:" << cake.at(9) << " IDR" << endl;
     cout <<"Topping\t: chocolatechips,strawberry,sprinkle\n\n\n";
-    
-    cout <<"10. " << cakeName.at(9) << endl;
+    cout <<"10. Tiramisu\n";
     cout << "An Italian dessert made from layers of ladyfinger biscuits soaked in coffee, mascarpone cream, and dusted with cocoa powder."<<endl;
     cout <<"Price\t:" << cake.at(10) << " IDR" << endl ;
     cout <<"Size\t: _________________________________ S/M/L/XL\n";
@@ -284,46 +266,50 @@ topping["hazelnuts"] = 5000;
     vector<string> cartCake;
     vector<string> cartSize;
     vector<string> cartTopping;
-    vector<string> cartTotal;
-    int Cake, Total = 0;
+    vector<int> cartTotal;
+    int Order = 0, Cake, Total = 0;
     string Fix, Size, Topping;
     Fix = "y";
     while(Fix == "y"){
        cout << "Choose your cake by the number: ";
        cin >> Cake;
        if(Cake > 0 && Cake < 11){
-         cartCake.push_back(cakeName.at(Cake));
+         cartCake.push_back(cakeName.at(Cake-1));
          Total += cake.at(Cake);
          bool isSize = false;
-         while(!isSize){
          cout << "Size\t: ";
          cin >> Size;
          LowerCase(Size);
+         while(!isSize){
          	if(Size == "s" ||  Size == "m" || Size == "l" || Size == "xl"){
                 isSize = true;
+                Order++;
                 Total += size.at(Size);
          		cartSize.push_back(Size);
                 
                 bool isTopping = false;
-                while(!isTopping){
          		cout << "Topping\t: ";
          		cin >> Topping;
                 LowerCase(Topping);
-                   if (std::find(cakeTopping.begin(), cakeTopping.end(), Topping) != cakeTopping.end()){
+        
+                while(!isTopping){
+                   if (find(cakeTopping.begin(), cakeTopping.end(), Topping) != cakeTopping.end()){
                        isTopping = true;
                        Total += topping.at(Topping);
          			   cartTopping.push_back(Topping);
+                       cartTotal.push_back(Total);
                    }
                    else{
-                       cout << "Topping is not on the chart.\n";
+                       cout << "Topping is not on the chart.";
                    }
                 }
          		
          	}
          	else{
-           		cout << "Size is not on the chart.\n";
+           		cout << "Size is not on the chart.";
          	}
-         }          
+         }
+           
        }
        else{
          cout << "Your cake is not in our list.";
@@ -332,33 +318,48 @@ topping["hazelnuts"] = 5000;
        cin >> Fix;
        LowerCase(Fix);
     }
+  	cout <<"===================================================\n";
+  	cout <<"====================| Bill |=======================\n\n\n";
+  	for(int i = 0; i < Order; i++){
+        cout << i+1 << ".\t " << cartCake[i] << " " << cartSize[i] << " " << cartTopping[i] << " " << cartTotal[i] << " IDR\n";
+        string transactionDetails = "Purchased " + cartCake[i] + " Size: " + cartSize[i] + " Topping: " + cartTopping[i] + " Total: " + to_string(cartTotal[i]) + " IDR";
+        currentUser.addTransaction(transactionDetails);
+    }
+    cout << "Transaction recorded.\n";
+  	
 }
 
-void TransactionHistory(){
-  //view
+
+void TransactionHistory(User& currentUser){
+    cout << "Transaction History for " << currentUser.getUserName() << ":\n";
+    currentUser.viewTransactionHistory();
 }
 
-void Homepage(){
+void Homepage(User& currentUser){
   char opt;
   cout << "1. Cake Catlogue\n";
+
   cout << "2. Transaction history\n";
   cout << "3. Log Out\n";
   cout << "Your Choice\t: ";
   cin >> opt;
   
   if(opt == '1'){
-    Catalogue();
+    Catalogue(currentUser);
   }
   else if(opt == '2'){
-   TransactionHistory();
+
+   TransactionHistory(currentUser);
   }
   else if(opt == '3'){
+
     return ;
   }
 }
 
 int main(){
   bool exit = false;
+  User currentUser("", "");
   cls();
   
   do{
@@ -373,7 +374,7 @@ int main(){
         cout << "\t\t\t\t         .|.          " << endl;
         cout << "\t\t\t\t         l7J          " << endl;
         cout << "\t\t\t\t         | |          " << endl;
-        cout << "\t\t\t\t     .--| |--.      " << endl;
+        cout << "\t\t\t\t     _.--| |--._      " << endl;
         cout << "\t\t\t\t  .-';  ;`-'& ; `&.   " << endl;
         cout << "\t\t\t\t & &  ;  &   ; ;   \\ " << endl;
         cout << "\t\t\t\t \\      ;    &   &_/ " << endl;
@@ -388,11 +389,15 @@ int main(){
     cin >> opt;
     switch(opt){
         case '1':
-            if(Login() == 1)Homepage();
+            if(Login(currentUser)){
+                Homepage(currentUser);
+            }
             break;
         case '2':
             SignUp();
-            if(Login() == 1)Homepage();
+            if(Login(currentUser)){
+                Homepage(currentUser);
+            }
             break;
       	case '3':
         	exit = true;
